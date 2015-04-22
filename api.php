7f3fb95f82b4903ceb6e7fb1d2ab52524d7b54ef
Matevz Vucnik <vucnikm@gmail.com>
2015-04-22 12:16:23 +0200

    add support for begin and end on the same date

--- api.php
+++ api.php
@@ -1,10 +1,10 @@
 <?php
 	include 'http-client.php';
-	
+
 	$fwdUrl="http://localhost/fwd.php";
-		
+
 	function dbQueryReturnId($dbconn, $sql) {
-		$result = pg_query($dbconn, $sql);	
+		$result = pg_query($dbconn, $sql);
 		if (!$result) {
 			die("An error occurred.");
 		}
@@ -13,7 +13,7 @@
 		while ($row = pg_fetch_row($result)) {
 			$id = $row[0];
 		}
-		
+
 		return $id;
 	}
 
@@ -24,26 +24,26 @@
 	}
 
 	set_error_handler('customError');
-	
+
 	$requestMethod = $_SERVER['REQUEST_METHOD'];
 	if ($requestMethod != 'POST') {
 		die("API only accepts POST method!");
 	}
 	$input = file_get_contents("php://input");
-	
+
 	//decoupling forwarding from storing data
 	sendPost($fwdUrl, $input);
-	
+
 	$json = json_decode($input);
 	$content = $json[0]->{'$content'};
-	$sensors = $content->sensors;	
+	$sensors = $content->sensors;
 	if (empty($sensors)) {
 		die ("Measurements missing!");
 	}
-	
-	$dbconn = pg_connect("host=localhost port=5433 dbname=test user=postgres")	
+
+	$dbconn = pg_connect("host=localhost port=5433 dbname=videk user=postgres")	
 		or die('Could not connect: ' . pg_last_error());
-	
+
 	$newNode = false;
 	$measurementCounter = 0;
 	foreach ($sensors as $sensor) {
@@ -55,25 +55,25 @@
 		$newSensor = false;
 		$newQuantity = false;
 		$newUnit = false;
-		
+
 		$nodeIdQuery = "INSERT INTO nodes (node_name) ";
 		$nodeIdQuery .= "SELECT '$node_name' ";
 		$nodeIdQuery .= "WHERE NOT EXISTS (";
 		$nodeIdQuery .= "SELECT '$node_name' ";
 		$nodeIdQuery .= "FROM nodes ";
-		$nodeIdQuery .= "WHERE node_name = '$node_name')";	
+		$nodeIdQuery .= "WHERE node_name = '$node_name')";
 		$nodeIdQuery .= "RETURNING node_id;";
-		
+
 		$nodeId = dbQueryReturnId($dbconn, $nodeIdQuery);
-		
+
 		if (!is_null($nodeId)) {
 			$newNode = true;
 		} else {
 			$nodeIdQuery = "SELECT node_id FROM nodes ";
 			$nodeIdQuery .= "WHERE node_name = '$node_name';";
-			$nodeId = dbQueryReturnId($dbconn, $nodeIdQuery);	
+			$nodeId = dbQueryReturnId($dbconn, $nodeIdQuery);
 		}
-		
+
 		$sensorIdQuery = "INSERT INTO sensors (sensor_name) ";
 		$sensorIdQuery .= "SELECT '$sensor_name' ";
 		$sensorIdQuery .= "WHERE NOT EXISTS (";
@@ -81,47 +81,47 @@
 		$sensorIdQuery .= "FROM sensors ";
 		$sensorIdQuery .= "WHERE sensor_name = '$sensor_name')";
 		$sensorIdQuery .= "RETURNING sensor_id;";
-		
+
 		$sensorId = dbQueryReturnId($dbconn, $sensorIdQuery);
-		
+
 		if (!is_null($sensorId)) {
-			$newSensor = true;			
+			$newSensor = true;
 		} else {
 			$sensorIdQuery = "SELECT sensor_id FROM sensors ";
 			$sensorIdQuery .= "WHERE sensor_name = '$sensor_name';";
-			$sensorId = dbQueryReturnId($dbconn, $sensorIdQuery);	
+			$sensorId = dbQueryReturnId($dbconn, $sensorIdQuery);
 		}
-		
+
 		if ($newNode or $newSensor)	 {
 			$snIdQuery = "INSERT INTO nodes_sensors (node_id,sensor_id) ";
 			$snIdQuery .= "VALUES ($nodeId,$sensorId);";
 			$nodeSensorId = dbQueryReturnId($dbconn, $snIdQuery);
 		}
-		
+
 		$quantityIdQuery = "INSERT INTO quantities (quantity_name) ";
 		$quantityIdQuery .= "SELECT '$quantity_name' ";
 		$quantityIdQuery .= "WHERE NOT EXISTS (";
 		$quantityIdQuery .= "SELECT '$quantity_name' ";
 		$quantityIdQuery .= "FROM quantities ";
 		$quantityIdQuery .= "WHERE quantity_name = '$quantity_name')";
-		$quantityIdQuery .= "RETURNING quantity_id;";		
-		
+		$quantityIdQuery .= "RETURNING quantity_id;";
+
 		$quantityId = dbQueryReturnId($dbconn, $quantityIdQuery);
-		
+
 		if (!is_null($quantityId)) {
-			$newQuantity = true;					
+			$newQuantity = true;
 		} else {
 			$quantityIdQuery = "SELECT quantity_id FROM quantities ";
 			$quantityIdQuery .= "WHERE quantity_name = '$quantity_name';";
-			$quantityId = dbQueryReturnId($dbconn, $quantityIdQuery);	
+			$quantityId = dbQueryReturnId($dbconn, $quantityIdQuery);
 		}
-		
+
 		if ($newSensor or $newQuantity)	 {
 			$quantityIdQuery = "INSERT INTO sensors_quantities (sensor_id,quantity_id) ";
 			$quantityIdQuery .= "VALUES ($sensorId,$quantityId);";
 			$sensorQuantityId = dbQueryReturnId($dbconn, $quantityIdQuery);
 		}
-		
+
 		$unitIdQuery = "INSERT INTO units (unit_name) ";
 		$unitIdQuery .= "SELECT '$quantity_unit' ";
 		$unitIdQuery .= "WHERE NOT EXISTS (";
@@ -129,23 +129,23 @@
 		$unitIdQuery .= "FROM units ";
 		$unitIdQuery .= "WHERE unit_name = '$quantity_unit')";
 		$unitIdQuery .= "RETURNING unit_id;";
-		
+
 		$unitId = dbQueryReturnId($dbconn, $unitIdQuery);
-		
+
 		if (!is_null($unitId)) {
-			$newUnit = true;			
+			$newUnit = true;
 		} else {
 			$unitIdQuery = "SELECT unit_id FROM units ";
 			$unitIdQuery .= "WHERE unit_name = '$quantity_unit';";
-			$unitId = dbQueryReturnId($dbconn, $unitIdQuery);				
+			$unitId = dbQueryReturnId($dbconn, $unitIdQuery);
 		}
-		
+
 		if ($newQuantity or $newUnit) {
 			$unitIdQuery = "INSERT INTO quantities_units (quantity_id,unit_id) ";
 			$unitIdQuery .= "VALUES ($quantityId,$unitId);";
 			$quantityUnitId = dbQueryReturnId($dbconn, $unitIdQuery);
-		}		
-		
+		}
+
 		$contextIdQuery = "INSERT INTO contexts (context_description) ";
 		$contextIdQuery .= "SELECT '$context_description' ";
 		$contextIdQuery .= "WHERE NOT EXISTS (";
@@ -153,10 +153,10 @@
 		$contextIdQuery .= "FROM contexts ";
 		$contextIdQuery .= "WHERE context_description = '$context_description');";
 		$contextIdQuery .= "SELECT context_id FROM contexts ";
-		$contextIdQuery .= "WHERE context_description = '$context_description';";		
-		
+		$contextIdQuery .= "WHERE context_description = '$context_description';";
+
 		$contextId = dbQueryReturnId($dbconn, $contextIdQuery);
-		
+
 		$measurementsIdQuery = "INSERT INTO measurements (node_id,";
 		$measurementsIdQuery .= "sensor_id,";
 		$measurementsIdQuery .= "quantity_id,";
@@ -166,38 +166,38 @@
 		$measurementsIdQuery .= "measurement_value,";
 		$measurementsIdQuery .= "unit_id) VALUES ";
 		$measurements = $sensor->measurements;
-		foreach ($measurements as $measurement) {			
+		foreach ($measurements as $measurement) {
 			$ts = gmdate("Y-m-d\TH:i:s\Z", $measurement->timestamp);
 			$lat = $measurement->latitude;
 			$long = $measurement->longitude;
 			$value = $measurement->value;
-		
+
 			$locationIdQuery = "INSERT INTO locations (latitude, longitude) ";
 			$locationIdQuery .= "SELECT $lat,$long ";
 			$locationIdQuery .= "WHERE NOT EXISTS (";
 			$locationIdQuery .= "SELECT $lat,$long ";
 			$locationIdQuery .= "FROM locations ";
 			$locationIdQuery .= "WHERE latitude = $lat AND longitude = $long)";
-			$locationIdQuery .= "RETURNING location_id;";		
-		
+			$locationIdQuery .= "RETURNING location_id;";
+
 			$locationId = dbQueryReturnId($dbconn, $locationIdQuery);
-		
+
 			if (!is_null($locationId)) {
 				$locationIdQuery = "INSERT INTO nodes_locations ";
 				$locationIdQuery .= "(node_id,location_id,node_location_ts) ";
 				$locationIdQuery .= "VALUES ($nodeId,$locationId,'$ts');";
-				$nodelocationId = dbQueryReturnId($dbconn, $locationIdQuery);					
+				$nodelocationId = dbQueryReturnId($dbconn, $locationIdQuery);
 			} else {
 				$locationIdQuery = "SELECT location_id FROM locations ";
 				$locationIdQuery .= "WHERE latitude = $lat AND longitude = $long";
-				$locationId = dbQueryReturnId($dbconn, $locationIdQuery);	
+				$locationId = dbQueryReturnId($dbconn, $locationIdQuery);
 			}
-			
-			$measurementsIdQuery .= "($nodeId,$sensorId,$quantityId,";	
-			$measurementsIdQuery .= "$locationId,$contextId,'$ts',$value,$unitId),";		
-			$measurementCounter++;			
+
+			$measurementsIdQuery .= "($nodeId,$sensorId,$quantityId,";
+			$measurementsIdQuery .= "$locationId,$contextId,'$ts',$value,$unitId),";
+			$measurementCounter++;
 		}
-		$measurementsIdQuery = substr_replace($measurementsIdQuery, ";", -1);		
+		$measurementsIdQuery = substr_replace($measurementsIdQuery, ";", -1);
 		$measurementId = dbQueryReturnId($dbconn, $measurementsIdQuery);
 	}
 	echo ($measurementCounter . ' measurements successfully uploaded!');
--- mbd.php
+++ mbd.php
@@ -8,7 +8,7 @@ function validateDate($date, $format = 'Y-m-d')
 date_default_timezone_set('UTC');
 $limit = 100000;
 if(array_key_exists("limit", $_GET)) {
-	$tmpLimit = $_GET['limit'];	
+	$tmpLimit = $_GET['limit'];
 	if(is_numeric($tmpLimit)) {
 		$tmpLimit += 0;
 		if(is_int($tmpLimit)) {
@@ -33,7 +33,7 @@ if(array_key_exists("ts-begin", $_GET)) {
 }
 
 if(array_key_exists("ts-end", $_GET)) {
-	$tsEnd = $_GET['ts-end'];	
+	$tsEnd = $_GET['ts-end'];
 	if(!validateDate($tsEnd)) {
 		die ("Error: Wrong end date format!");
 	}
@@ -41,9 +41,14 @@ if(array_key_exists("ts-end", $_GET)) {
 	die ("Error: End date missing!");
 }
 
+if($tsBegin == $tsEnd) {
+  $tsBegin .= " 00:00:00";
+  $tsEnd .= " 23:59:59";
+}
+
 $timezone = 0;
 if(array_key_exists("tz", $_GET)) {
-	$tmpTz = $_GET['tz'];	
+	$tmpTz = $_GET['tz'];
 	if(is_numeric($tmpTz)) {
 		$tmpTz += 0;
 		if(is_int($tmpTz)) {
@@ -52,10 +57,10 @@ if(array_key_exists("tz", $_GET)) {
 	}
 }
 
-if (!empty($sn_id) && strlen($sn_id) <= 5) {
-	$dbconn = pg_connect("host=localhost port=5433 dbname=videk user=postgres")	
+if (!empty($sn_id) && strlen($sn_id) <= 7) {
+	$dbconn = pg_connect("host=localhost port=5433 dbname=videk user=postgres")
 		or die('Could not connect: ' . pg_last_error());
-	
+
 	$query = "SELECT measurement_id,node_name,sensor_name,";
 	$query .= "quantity_name,unit_name,context_description,latitude,longitude,";
 	$query .= "EXTRACT(epoch FROM measurement_ts AT TIME ZONE 'UTC'),";
@@ -68,27 +73,27 @@ if (!empty($sn_id) && strlen($sn_id) <= 5) {
 	$query .= "measurements.unit_id = units.unit_id AND ";
 	$query .= "measurements.context_id = contexts.context_id AND ";
 	$query .= "measurements.location_id = locations.location_id AND ";
-	$query .= "nodes.node_name = '$sn_id' AND ";	
+	$query .= "nodes.node_name = '$sn_id' AND ";
 	if ($limit == 0) {
 		$query .= "measurement_ts BETWEEN '$tsBegin' AND '$tsEnd';";
 	} else {
 		$query .= "measurement_ts BETWEEN '$tsBegin' AND '$tsEnd'";
 		$query .= "LIMIT $limit;";
 	}
-	
-	$result = pg_query($dbconn, $query);	
+
+	$result = pg_query($dbconn, $query);
 	if (!$result) {
 		die("An error occurred.");
 	} else if (pg_num_rows ($result) == 0) {
 		die ("Error: No measurements for the input!");
-	} else {		
+	} else {
 		header("Content-Disposition: attachment; filename=\"$sn_id.csv\"");
 		$firstLine = "node_name,sensor_name,quantity_name,unit_name,";
 		$firstLine .= "context_description,latitude,longitude,measurement_timestamp,";
 		$firstLine .= "measurement_value\r\n";
 		echo $firstLine;
 		while ($row = pg_fetch_row($result)) {
-			$ts = date('Y-m-d H:i:s', $row[8] + $timezone*3600);		
+			$ts = date('Y-m-d H:i:s', $row[8] + $timezone*3600);
 			$line = $row[1] . "," . $row[2] . ",";
 			$line .= $row[3] . "," . $row[4] . ",";
 			$line .= $row[5] . "," . $row[6]. ",";
